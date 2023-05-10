6bcd1fdfa75ab1f9b2ee60cf7ac0fccfc5e259c8
Aleksandr Nimko <shurick2211@gmail.com>
2023-05-10 15:16:08 +0300

    main fragment

--- app/src/main/java/com/nimko/contacts_from_api/MainActivity.kt
+++ app/src/main/java/com/nimko/contacts_from_api/MainActivity.kt
@@ -1,114 +1,34 @@
 package com.nimko.contacts_from_api
 
-import android.app.Activity
-import android.content.Intent
 import android.content.pm.PackageManager
 import android.os.Bundle
 import android.util.Log
-import android.view.Menu
-import android.view.View
-import android.widget.SearchView
 import android.widget.Toast
-import androidx.activity.result.ActivityResult
-import androidx.activity.result.ActivityResultLauncher
-import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
-import androidx.lifecycle.ViewModelProvider
-import androidx.recyclerview.widget.LinearLayoutManager
-import com.nimko.contacts_from_api.adapter.MyItemRecyclerViewAdapter
 import com.nimko.contacts_from_api.databinding.ActivityMainBinding
-import com.nimko.contacts_from_api.model.ItemForAdapter
-import com.nimko.contacts_from_api.model.MyViewModel
+import com.nimko.contacts_from_api.fragment.MainFragment
 
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-    private val adapter: MyItemRecyclerViewAdapter = MyItemRecyclerViewAdapter()
-    private var startForResult: ActivityResultLauncher<Intent>? = null
-    private lateinit var model: MyViewModel
+
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        supportActionBar?.hide()
 
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragmentContainerView, MainFragment.newInstance())
+            .commit()
         checkPermission()
-
-        startForResult = registerForActivityResult(ActivityResultContracts.StartActivityForResult())
-        { result: ActivityResult ->
-            if (result.resultCode == Activity.RESULT_OK) {
-                val intent = result.data
-                val person = intent?.getSerializableExtra("new_person") as ItemForAdapter.Person
-                Log.d("MainActivity result", person.toString())
-            }
-        }
-
-        binding.list.layoutManager = LinearLayoutManager(this)
-        binding.list.adapter = adapter
-
-        model = ViewModelProvider(this).get(MyViewModel::class.java)
-        model.values.observe(this, {
-            refreshList(it)
-            Log.d("MainActivity", "Observer")
-        })
     }
 
-    private fun progressBar(){
-        val waitProgres = ItemForAdapter.Header("", true)
-        adapter.values.add(0, waitProgres)
-        adapter.refresh()
-    }
-
-    override fun onResume() {
-        super.onResume()
-        progressBar()
-        model.apiReq()
-    }
-    private fun refreshList(allContacts: MutableList<ItemForAdapter.Person>) {
-        allContacts.sortBy { it.firstName }
-        var ch = allContacts[0].firstName[0]
-        adapter.values.clear()
-        adapter.values.add(ItemForAdapter.Header(ch.toString(), false))
-        allContacts.forEach {
-            val startCh = it.firstName[0]
-            if (startCh != ch) {
-                adapter.values.add(ItemForAdapter.Header(startCh.toString(),false))
-                ch = startCh
-            }
-            adapter.values.add(it)
-        }
-        adapter.refresh()
-        Log.d("List", "Refresh")
-    }
-
-
-    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        menuInflater.inflate(R.menu.menu, menu)
-        val searchItem = menu?.findItem(R.id.app_bar_search)
-        val searchView = searchItem?.actionView as SearchView
-        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
-            override fun onQueryTextSubmit(query: String?): Boolean {
-                return false
-            }
-            override fun onQueryTextChange(ch: String?): Boolean {
-                if (!ch.isNullOrBlank()) {
-                    adapter.values = model.find(ch).toMutableList()
-                    adapter.refresh()
-                } else {refreshList(model.find(null))}
-                return true
-            }
-        })
-        return true
-    }
-
-
-    fun onClickAdd(view: View) {
-        startForResult?.launch(Intent(this, EditActivity::class.java))
-    }
 
 
     private fun checkPermission() {
---
+++ app/src/main/java/com/nimko/contacts_from_api/fragment/MainFragment.kt
@@ -0,0 +1,126 @@
+package com.nimko.contacts_from_api.fragment
+
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.Menu
+import android.view.View
+import android.view.ViewGroup
+import android.widget.SearchView
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.nimko.contacts_from_api.EditActivity
+import com.nimko.contacts_from_api.R
+import com.nimko.contacts_from_api.adapter.MyItemRecyclerViewAdapter
+import com.nimko.contacts_from_api.databinding.FragmentMainBinding
+import com.nimko.contacts_from_api.model.ItemForAdapter
+import com.nimko.contacts_from_api.model.MyViewModel
+
+
+class MainFragment : Fragment() {
+
+    private lateinit var binding:FragmentMainBinding
+    private val adapter: MyItemRecyclerViewAdapter = MyItemRecyclerViewAdapter()
+    private var startForResult: ActivityResultLauncher<Intent>? = null
+    private lateinit var model: MyViewModel
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentMainBinding.inflate(inflater,container,false)
+        binding.toolbar.inflateMenu(R.menu.menu)
+        binding.toolbar.menu.apply {
+            onCreateOptionsMenu(this)
+        }
+        binding.buttonAdd.setOnClickListener {
+            startForResult?.launch(Intent(activity, EditActivity::class.java))
+        }
+
+        startForResult = registerForActivityResult(ActivityResultContracts.StartActivityForResult())
+        { result: ActivityResult ->
+            if (result.resultCode == Activity.RESULT_OK) {
+                val intent = result.data
+                val person = intent?.getSerializableExtra("new_person") as ItemForAdapter.Person
+                Log.d("MainActivity result", person.toString())
+            }
+        }
+
+        binding.list.layoutManager = LinearLayoutManager(context)
+        binding.list.adapter = adapter
+
+        model = ViewModelProvider(this).get(MyViewModel::class.java)
+        model.values.observe(this, {
+            refreshList(it)
+            Log.d("MainActivity", "Observer")
+        })
+
+        return binding.root
+    }
+
+    private fun progressBar(){
+        val waitProgres = ItemForAdapter.Header("", true)
+        adapter.values.add(0, waitProgres)
+        adapter.refresh()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        progressBar()
+        model.apiReq()
+    }
+
+    private fun refreshList(allContacts: MutableList<ItemForAdapter.Person>) {
+        allContacts.sortBy { it.firstName }
+        var ch = allContacts[0].firstName[0]
+        adapter.values.clear()
+        adapter.values.add(ItemForAdapter.Header(ch.toString(), false))
+        allContacts.forEach {
+            val startCh = it.firstName[0]
+            if (startCh != ch) {
+                adapter.values.add(ItemForAdapter.Header(startCh.toString(),false))
+                ch = startCh
+            }
+            adapter.values.add(it)
+        }
+        adapter.refresh()
+        Log.d("List", "Refresh")
+    }
+
+
+    fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val searchItem = menu?.findItem(R.id.app_bar_search)
+        val searchView = searchItem?.actionView as SearchView
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                return false
+            }
+            override fun onQueryTextChange(ch: String?): Boolean {
+                if (!ch.isNullOrBlank()) {
+                    adapter.values = model.find(ch).toMutableList()
+                    adapter.refresh()
+                } else {refreshList(model.find(null))}
+                return true
+            }
+        })
+        return true
+    }
+
+
+    companion object {
+        @JvmStatic
+        fun newInstance() =
+            MainFragment()
+    }
+}
